379a63ac7019f8df55ccc2ee743f66eb016d23f0
gh1231002 <gh1231002@naver.com>
2024-03-05 19:50:11 +0900

    3월 5일

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float PlayerSpeed = 0.0f; //플레이어 이동속도
+    Camera cam;
+    Animator anim;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
